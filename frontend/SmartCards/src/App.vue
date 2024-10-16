c7e056a1c975b32e1e2364d2c0c6f894a2550872
Jude <108977329+JuDoKai@users.noreply.github.com>
2024-10-16 22:58:25 +0200

    Ajout Connexion Component

---
+++ frontend/SmartCards/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="main-container">
+    <div class="main-bg">
+
+    </div>
+  </div>
+
+  <Sign_Up/>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import Sign_Up from './Sign_Up.vue';
+
+const todos = ref([])
+const newTodo = ref('')
+
+const addTodo = () => {
+  todos.value.push({
+    title: newTodo.value,
+    completed: false,
+    date: Date.now()
+  })
+  newTodo.value = ''
+}
+</script>
+
+<style>
+
+
+
+.main-container{
+  display: flex;
+  flex-direction: row;
+}
+
+.main-bg{
+  height: 50%;
+  width: 50%;
+  background-image: url(/src/images/logo.png);
+  background-size: contain;
+  background-repeat: no-repeat;
+  border-radius: 40px;
+}
+  
+
+</style>
---
+++ frontend/SmartCards/src/Checkbox.vue
@@ -0,0 +1,12 @@
+<template>
+    <label type="checkbox">
+        {{ label }} 
+    </label>
+</template>
+
+<script setup>
+const props = defineProps({
+    label: String
+})
+console.log(props)
+</script>
